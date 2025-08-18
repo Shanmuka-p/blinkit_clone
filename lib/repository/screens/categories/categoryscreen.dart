6378d7a46cf4294f572f8768431fbcf5722e7f5e
Shanmuka-p <shanmukareddypadala@gmail.com>
2025-08-18 20:27:23 +0530

    category screen app bar

--- lib/repository/screens/categories/categoryscreen.dart
+++ lib/repository/screens/categories/categoryscreen.dart
@@ -1,3 +1,4 @@
+import 'package:blinkit_clone/repository/widgets/uihelper.dart';
 import 'package:flutter/material.dart';
 
 class Categoryscreen extends StatefulWidget {
@@ -11,6 +12,93 @@ class _CategoryscreenState extends State<Categoryscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 180,
+        backgroundColor: Color(0XFFF7CB45),
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Uihelper.CustomText(
+              text: "BlinkIt in",
+              color: Colors.black,
+              FontWeight: FontWeight.w700,
+              fontsize: 12,
+              fontfamily: "bold",
+            ),
+            Uihelper.CustomText(
+              text: "16 minutes",
+              color: Colors.black,
+              FontWeight: FontWeight.w700,
+              fontsize: 20,
+              fontfamily: "bold",
+            ),
+
+            Row(
+              children: [
+                Uihelper.CustomText(
+                  text: "HOME - ",
+                  color: Colors.black,
+                  FontWeight: FontWeight.bold,
+                  fontsize: 12,
+                  fontfamily: "bold",
+                ),
+                Uihelper.CustomText(
+                  text: "Shanmuka Reddy, G.mamidada - ",
+                  color: Colors.black,
+                  FontWeight: FontWeight.w400,
+                  fontsize: 12,
+                ),
+                Image.asset("assets/images/arrow_down.png"),
+                Padding(
+                  padding: const EdgeInsets.only(left: 70),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      color: Colors.white,
+                    ),
+                    child: Icon(Icons.person, size: 35),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.horizontal(
+                  left: Radius.circular(10),
+                  right: Radius.circular(10),
+                ),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.horizontal(
+                      left: Radius.circular(10),
+                      right: Radius.circular(10),
+                    ),
+                  ),
+                  prefixIcon: Image.asset(
+                    "assets/images/search.png",
+                    color: Colors.black,
+                  ),
+                  hintText: "Search “ice-cream”",
+                  hintStyle: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    fontFamily: "bold",
+                  ),
+                  suffixIcon: Image.asset(
+                    "assets/images/mic.png",
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Center(child: Text("Category Screen")),
     );
   }
