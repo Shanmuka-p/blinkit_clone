c6440a843c7408fe73536857d6c1f57f19a39fab
Shanmuka-p <shanmukareddypadala@gmail.com>
2025-08-31 21:20:55 +0530

    Login with number

--- lib/repository/screens/login/number.dart
+++ lib/repository/screens/login/number.dart
@@ -1,3 +1,5 @@
+import 'package:blinkit_clone/domain/constants/appcolors.dart';
+import 'package:blinkit_clone/repository/widgets/uihelper.dart';
 import 'package:flutter/material.dart';
 
 class Number extends StatefulWidget {
@@ -10,6 +12,49 @@ class Number extends StatefulWidget {
 class _NumberState extends State<Number> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Appcolors.Scaffoldbackground,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Uihelper.CustomText(
+                text: "Enter Your Number",
+                color: Colors.black,
+                FontWeight: FontWeight.w700,
+                fontsize: 20,
+                fontfamily: "bold",
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(16)),
+                ),
+                child: TextFormField(
+                  //textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    //slabelText: "Enter the number",
+                    alignLabelWithHint: false,
+                    disabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    hintText: "Enter the number",
+                    hintTextDirection: TextDirection.ltr,
+                  ),
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
