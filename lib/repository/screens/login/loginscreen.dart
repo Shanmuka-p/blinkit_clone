1f305f197b2650f59a237732613d0cb1e791ff5f
Shanmuka-p <shanmukareddypadala@gmail.com>
2025-09-09 21:24:32 +0530

    Map screen

--- lib/repository/screens/login/loginscreen.dart
+++ lib/repository/screens/login/loginscreen.dart
@@ -44,93 +44,96 @@ class Loginscreen extends StatelessWidget {
                 borderRadius: BorderRadius.circular(10),
                 color: Colors.white,
               ),
-              child: Column(
-                children: [
-                  Uihelper.customText(
-                    text: "Shanmuka Reddy",
-                    color: Colors.black,
-                    fontWeight: FontWeight.w100,
-                    fontsize: 15,
-                  ),
-                  SizedBox(height: 10),
-                  Uihelper.customText(
-                    text: "628131XXXX",
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontsize: 15,
-                    fontfamily: "bold",
-                  ),
-                  SizedBox(height: 20),
-                  SizedBox(
-                    height: 40,
-                    width: 295,
-                    child: ElevatedButton(
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Column(
+                  children: [
+                    // Uihelper.customText(
+                    //   text: "Shanmuka Reddy",
+                    //   color: Colors.black,
+                    //   fontWeight: FontWeight.w100,
+                    //   fontsize: 15,
+                    // ),
+                    // SizedBox(height: 10),
+                    // Uihelper.customText(
+                    //   text: "628131XXXX",
+                    //   color: Colors.black,
+                    //   fontWeight: FontWeight.bold,
+                    //   fontsize: 15,
+                    //   fontfamily: "bold",
+                    // ),
+                    SizedBox(height: 20),
+                    SizedBox(
+                      height: 40,
+                      width: 295,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SalomanBottomBar(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0XFFE23744),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Uihelper.customText(
+                              text: "Login  with",
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                              fontsize: 14,
+                            ),
+                            SizedBox(width: 5),
+                            Uihelper.customImage(
+                              img: "zomato.png",
+                              height: 40,
+                              width: 80,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Uihelper.customText(
+                      text:
+                          "Access your saved addresses from Zomato automatically!",
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                      fontsize: 10,
+                    ),
+                    SizedBox(height: 10),
+                    TextButton(
                       onPressed: () {
                         Navigator.pushReplacement(
                           context,
-                          MaterialPageRoute(
-                            builder: (context) => SalomanBottomBar(),
-                          ),
+                          MaterialPageRoute(builder: (context) => Number()),
                         );
                       },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0XFFE23744),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
+                      child: Text(
+                        "or login with phone number",
+                        style: TextStyle(
+                          color: Color(0XFF269237),
+                          fontSize: 17,
+                          fontWeight: FontWeight.w400,
                         ),
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Uihelper.customText(
-                            text: "Login  with",
-                            color: Colors.white,
-                            fontWeight: FontWeight.w700,
-                            fontsize: 14,
-                          ),
-                          SizedBox(width: 5),
-                          Uihelper.customImage(
-                            img: "zomato.png",
-                            height: 40,
-                            width: 80,
-                          ),
-                        ],
-                      ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Uihelper.customText(
-                    text:
-                        "Access your saved addresses from Zomato automatically!",
-                    color: Colors.black,
-                    fontWeight: FontWeight.w400,
-                    fontsize: 10,
-                  ),
-                  //SizedBox(height: 10),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => Number()),
-                      );
-                    },
-                    child: Text(
-                      "or login with phone number",
-                      style: TextStyle(
-                        color: Color(0XFF269237),
-                        fontSize: 17,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ),
 
-                  // Uihelper.customText(
-                  //   text: "or login with phone number",
-                  //   color: Color(0XFF269237),
-                  //   fontWeight: FontWeight.w400,
-                  //   fontsize: 17,
-                  // ),
-                ],
+                    // Uihelper.customText(
+                    //   text: "or login with phone number",
+                    //   color: Color(0XFF269237),
+                    //   fontWeight: FontWeight.w400,
+                    //   fontsize: 17,
+                    // ),
+                  ],
+                ),
               ),
             ),
           ),
